27d97ceaf55f821bda04352e08fac3e997341a5e
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-05-31 02:09:03 -0700

    Update model.py

--- model.py
+++ model.py
@@ -10,16 +10,18 @@ class NeuralNetwork(nn.Module):
         self.linear3=nn.Linear(num_hidden2, num_hidden3)
         self.logits=nn.Linear(num_hidden3, output_size)
         self.dropout=nn.Dropout(0.2)
-        self.batchnorm=nn.BatchNorm3d()
+        self.batchnorm1=nn.BatchNorm1d(num_hidden1)
+        self.batchnorm2=nn.BatchNorm1d(num_hidden2)
+        self.batchnorm3=nn.BatchNorm1d(num_hidden3)
         self.relu=nn.ReLU()
 
     def forward(self,x):
         layer1=self.linear1(x)
-        act1=self.dropout(self.relu(layer1))
+        act1=self.dropout(self.batchnorm1(self.relu(layer1)))
         layer2=self.linear2(act1)
-        act2=self.dropout(self.relu(layer2))
+        act2=self.dropout(self.batchnorm2(self.relu(layer2)))
         layer3=self.linear3(act2)
-        act3=self.dropout(self.relu(layer3))
+        act3=self.dropout(self.batchnorm3(self.relu(layer3)))
         out=self.logits(act3)
 
         return out
